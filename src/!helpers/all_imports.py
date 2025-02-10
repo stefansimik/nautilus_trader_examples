d7b3a3dc83f9c638926d387f6f8bba2f707035c5
Stefan Simik <stefan.simik@gmail.com>
2025-02-10 19:14:45 +0100

    All imports updated to prioritize rules in __init__.py in modules.

--- src/!helpers/all_imports.py
+++ src/!helpers/all_imports.py
@@ -22,9 +22,10 @@ from nautilus_trader.analysis.statistics.loser_avg import AvgLoser
 from nautilus_trader.analysis.statistics.win_rate import WinRate
 from nautilus_trader.analysis.statistics.winner_avg import AvgWinner
 from nautilus_trader.backtest.engine import BacktestEngine, BacktestEngineConfig
-from nautilus_trader.backtest.models import (FeeModel, FillModel, FixedFeeModel, MakerTakerFeeModel, PerContractFeeModel)
-from nautilus_trader.cache.cache import Cache
-from nautilus_trader.common.actor import Actor, ActorConfig, ActorExecutor, ImportableActorConfig
+from nautilus_trader.backtest.models import (
+    FeeModel, FillModel, FixedFeeModel, MakerTakerFeeModel, PerContractFeeModel)
+from nautilus_trader.cache import Cache, CacheDatabaseAdapter
+from nautilus_trader.common import Environment, Actor, ActorExecutor
 from nautilus_trader.common.component import LiveClock, MessageBus, TestClock, TimeEvent
 from nautilus_trader.common.config import NautilusConfig
 from nautilus_trader.common.enums import ComponentState, ComponentTrigger, LogColor, LogLevel
@@ -32,15 +33,18 @@ from nautilus_trader.common.factories import OrderFactory
 from nautilus_trader.common.functions import format_utc_timerange
 from nautilus_trader.common.providers import InstrumentProvider
 from nautilus_trader.config import (
-    ActorConfig, ActorFactory, BacktestDataConfig, BacktestEngineConfig, BacktestRunConfig, BacktestVenueConfig,
-    CacheConfig, ControllerConfig, ControllerFactory, DataCatalogConfig, DataEngineConfig, DatabaseConfig,
-    ExecAlgorithmConfig, ExecAlgorithmFactory, ExecEngineConfig, FXRolloverInterestConfig, ImportableActorConfig,
-    ImportableConfig, ImportableControllerConfig, ImportableExecAlgorithmConfig, ImportableStrategyConfig,
-    InstrumentProviderConfig, InvalidConfiguration, LiveDataClientConfig, LiveDataEngineConfig, LiveExecClientConfig,
-    LiveExecEngineConfig, LiveRiskEngineConfig, LoggingConfig, MessageBusConfig, NautilusConfig, NautilusKernelConfig,
-    NonNegativeFloat, NonNegativeInt, OrderEmulatorConfig, PositiveFloat, PositiveInt, RiskEngineConfig, RoutingConfig,
-    SimulationModuleConfig, StrategyConfig, StrategyFactory, StreamingConfig, TradingNodeConfig
+    ActorConfig, ActorFactory, BacktestDataConfig, BacktestEngineConfig, BacktestRunConfig,
+    BacktestVenueConfig, CacheConfig, ControllerConfig, ControllerFactory, DataCatalogConfig,
+    DataEngineConfig, DatabaseConfig, ExecAlgorithmConfig, ExecAlgorithmFactory, ExecEngineConfig,
+    FXRolloverInterestConfig, ImportableActorConfig, ImportableConfig, ImportableControllerConfig,
+    ImportableExecAlgorithmConfig, ImportableStrategyConfig, InstrumentProviderConfig,
+    InvalidConfiguration, LiveDataClientConfig, LiveDataEngineConfig, LiveExecClientConfig,
+    LiveExecEngineConfig, LiveRiskEngineConfig, LoggingConfig, MessageBusConfig,
+    NautilusConfig, NautilusKernelConfig, NonNegativeFloat, OrderEmulatorConfig, RiskEngineConfig,
+    RoutingConfig, SimulationModuleConfig, StrategyConfig, StrategyFactory, StreamingConfig,
+    TradingNodeConfig,
 )
+from nautilus_trader.core import UUID4, Command, Data, Document, Event, Request, Response
 from nautilus_trader.core.correctness import PyCondition
 from nautilus_trader.core.data import Data
 from nautilus_trader.core.datetime import (
@@ -49,9 +53,7 @@ from nautilus_trader.core.datetime import (
     unix_nanos_to_dt
 )
 from nautilus_trader.core.fsm import FiniteStateMachine, InvalidStateTrigger
-from nautilus_trader.core.message import Event
-from nautilus_trader.core.uuid import UUID4
-from nautilus_trader.data.client import (DataClient, MarketDataClient)
+from nautilus_trader.data.client import DataClient, MarketDataClient
 from nautilus_trader.indicators.amat import ArcherMovingAveragesTrends
 from nautilus_trader.indicators.aroon import AroonOscillator
 from nautilus_trader.indicators.atr import AverageTrueRange
@@ -88,7 +90,19 @@ from nautilus_trader.indicators.swings import Swings
 from nautilus_trader.indicators.vhf import VerticalHorizontalFilter
 from nautilus_trader.indicators.volatility_ratio import VolatilityRatio
 from nautilus_trader.indicators.vwap import VolumeWeightedAveragePrice
-from nautilus_trader.model.book import BookLevel, OrderBook
+from nautilus_trader.live.config import (
+    LiveDataClientConfig, LiveRiskEngineConfig, LiveExecEngineConfig, RoutingConfig,
+    LiveExecClientConfig, ControllerConfig, ControllerFactory, TradingNodeConfig)
+from nautilus_trader.live.data_client import LiveDataClient, LiveMarketDataClient
+from nautilus_trader.live.data_engine import LiveDataEngine
+from nautilus_trader.live.execution_client import LiveExecutionClient
+from nautilus_trader.live.execution_engine import LiveExecutionEngine
+from nautilus_trader.live.factories import LiveDataClientFactory, LiveExecClientFactory
+from nautilus_trader.live.node import TradingNode
+from nautilus_trader.live.node_builder import TradingNodeBuilder
+from nautilus_trader.live.risk_engine import LiveRiskEngine
+
+
 from nautilus_trader.model.currencies import (
     # Fiat Currencies:
     AUD, BRL, CAD, CHF, CNY, CNH, CZK, DKK, EUR, GBP, HKD, HUF, ILS, INR, JPY, KRW, MXN, NOK, NZD,
@@ -98,22 +112,40 @@ from nautilus_trader.model.currencies import (
     ETHW, EZ, FTT, JOE, LINK, LTC, LUNA, NBT, SOL, TRX, TRYB, TUSD, VTC, XLM, XMR, XRP, XTZ, USDC,
     USDC_POS, USDP, USDT, WSB, XEC, ZEC,
 )
-from nautilus_trader.model.data import (
-    Bar, BarSpecification, BarType, DataType, CustomData,
-    BookOrder, OrderBookDelta, OrderBookDeltas, OrderBookDepth10, InstrumentStatus, InstrumentClose,
-    QuoteTick, TradeTick
+
+from nautilus_trader.config import (
+    AccountBalance, AccountId, Bar, BarSpecification, BarType, BookLevel, BookOrder, ClientId,
+    ClientOrderId, ComponentId, Currency, CustomData, DataType, ExecAlgorithmId, InstrumentClose,
+    InstrumentId, InstrumentStatus, MarginBalance, Money, OrderBook, OrderBookDelta,
+    OrderBookDeltas, OrderBookDepth10, OrderListId, Position, PositionId, Price, Quantity,
+    QuoteTick, StrategyId, Symbol, TradeId, TradeTick, TraderId, Venue, VenueOrderId,
 )
+
+
+from nautilus_trader.model.events import (
+    AccountState, OrderAccepted, OrderCancelRejected, OrderCanceled, OrderDenied, OrderEmulated, OrderExpired,
+    OrderFilled, OrderInitialized, OrderModifyRejected, OrderPendingCancel, OrderPendingUpdate, OrderRejected,
+    OrderReleased, OrderSubmitted, OrderTriggered, OrderUpdated, PositionChanged, PositionClosed, PositionOpened
+)
+
+from nautilus_trader.model.instruments import (
+    BettingInstrument, BinaryOption,  Cfd, Commodity, CryptoFuture, CryptoPerpetual,
+    CurrencyPair, Equity, FuturesContract, FuturesSpread, IndexInstrument, Instrument,
+    OptionContract, OptionSpread, SyntheticInstrument
+)
+
+from nautilus_trader.model.orders import (
+    LimitIfTouchedOrder, LimitOrder, MarketIfTouchedOrder, MarketOrder, MarketToLimitOrder, Order, StopLimitOrder,
+    StopMarketOrder, TrailingStopLimitOrder, TrailingStopMarketOrder
+)
+
 from nautilus_trader.model.enums import (
     AccountType, AggregationSource, AggressorSide, AssetClass, BarAggregation, BookAction, BookType, ContingencyType,
     CurrencyType, InstrumentClass, InstrumentCloseType, LiquiditySide, MarketStatus, MarketStatusAction, OmsType,
     OptionKind, OrderSide, OrderStatus, OrderType, PositionSide, PriceType, RecordFlag, TimeInForce, TradingState,
     TrailingOffsetType, TriggerType
 )
-from nautilus_trader.model.events import (
-    AccountState, OrderAccepted, OrderCancelRejected, OrderCanceled, OrderDenied, OrderEmulated, OrderExpired,
-    OrderFilled, OrderInitialized, OrderModifyRejected, OrderPendingCancel, OrderPendingUpdate, OrderRejected,
-    OrderReleased, OrderSubmitted, OrderTriggered, OrderUpdated, PositionChanged, PositionClosed, PositionOpened
-)
+
 from nautilus_trader.model.functions import (
     account_type_to_str, aggregation_source_to_str, aggressor_side_to_str, asset_class_to_str,
     instrument_class_to_str, bar_aggregation_to_str, book_action_to_str, book_type_to_str,
@@ -122,20 +154,44 @@ from nautilus_trader.model.functions import (
     option_kind_to_str, order_type_to_str, record_flag_to_str, time_in_force_to_str,
     trading_state_to_str, trailing_offset_type_to_str, trigger_type_to_str,
 )
+
 from nautilus_trader.model.identifiers import (
     AccountId, ClientId, ClientOrderId, InstrumentId, OrderListId, PositionId, StrategyId, Symbol, TradeId, TraderId,
     Venue, VenueOrderId
 )
-from nautilus_trader.model.instruments import (FuturesContract, Instrument)
-from nautilus_trader.model.objects import AccountBalance, MarginBalance, Money, Price, Quantity
-from nautilus_trader.model.orders import (
-    LimitIfTouchedOrder, LimitOrder, MarketIfTouchedOrder, MarketOrder, MarketToLimitOrder, Order, StopLimitOrder,
-    StopMarketOrder, TrailingStopLimitOrder, TrailingStopMarketOrder
+
+from nautilus_trader.model.objects import (
+    Quantity, Price, Money, Currency, AccountBalance, MarginBalance
 )
-from nautilus_trader.model.orders.list import OrderList
+
 from nautilus_trader.model.position import Position
-from nautilus_trader.persistence.catalog.parquet import ParquetDataCatalog
-from nautilus_trader.persistence.wranglers import (
-    BarDataWrangler, OrderBookDeltaDataWrangler, QuoteTickDataWrangler, TradeTickDataWrangler
+
+from nautilus_trader.model.venues import (
+    CBCM, GLBX, NYUM, XCBT, XCEC, XCME, XFXS, XNYM
 )
-from nautilus_trader.trading.strategy import Strategy
+
+from nautilus_trader.persistence.catalog import BaseDataCatalog, ParquetDataCatalog
+
+from nautilus_trader.persistence.loaders import (
+    CSVTickDataLoader, CSVBarDataLoader, ParquetTickDataLoader, ParquetBarDataLoader,
+    InterestRateProviderConfig, InterestRateProvider
+)
+
+from nautilus_trader.persistence.wranglers import (
+    BarDataWrangler, TradeTickDataWrangler, QuoteTickDataWrangler, OrderBookDeltaDataWrangler
+)
+
+from nautilus_trader.persistence.wranglers_v2 import (
+    BarDataWranglerV2, TradeTickDataWranglerV2, QuoteTickDataWranglerV2, OrderBookDeltaDataWranglerV2
+)
+
+from nautilus_trader.portfolio import Portfolio, PortfolioFacade
+
+from nautilus_trader.risk.engine import RiskEngine
+from nautilus_trader.risk.sizing import PositionSizer, FixedRiskSizer
+
+from nautilus_trader.trading import Controller, Strategy, Trader
+
+from nautilus_trader.trading.filters import (
+    ForexSession, ForexSessionFilter, NewsImpact, NewsEvent, EconomicNewsEventFilter
+)
